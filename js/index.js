baa68b592bdbfd3630995f5eb8b8753adb74676e
nbowd <bowdenn@oregonstate.edu>
2021-08-23 21:39:33 -0700

    html overhaul

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const navToggle = document.getElementById('nav-btn');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
--- src/components/data/intro.js
+++
@@ -1,3 +0,0 @@
-export const Intro = [{intro:`Hello there! My name is Nick Bowden.
- I'm a Web Developer currently based in Sequim, WA. 
- Get in touch and let's build something unique`}]
